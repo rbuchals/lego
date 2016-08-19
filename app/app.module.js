ffb9c54c8cfbcf9124c0faa347292c98ec9d7dba
rbuchals <robert.buchalski@gmail.com>
2016-08-19 07:24:35 +0200

    3-bootstrap. Refactoring: layout moved;

--- app/app.module.js
+++ app/app.module.js
@@ -2,4 +2,7 @@ angular.module('app', ['ngRoute',  'app.main'])
     .config(function ($locationProvider) {
         'use strict';
         $locationProvider.html5Mode(false);
-    });
+    })
+    .controller('AppCntl', ['$scope', function($scope) {
+        $scope.message = 'Hello!';
+    }]);;
